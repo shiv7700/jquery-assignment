fb7644b3c71324c0a461d60ae0966240d604fe17
ShivanG <shivangramola007@gmail.com>
2023-12-12 14:42:11 +0530

    new

---
+++ add-another/main.js
@@ -0,0 +1,26 @@
+// add functionality
+
+$(".add-another").click(function () {
+  let num = new Date().getTime();
+  const div = document.createElement("div");
+  div.innerHTML = `        <input type="file">
+        <select>
+            <option value="">primary</option>
+            <option value="">secondary</option>
+        </select>
+        <div class="remove">Remove</div>`;
+  div.className = `${num}`;
+  $(".result").append(div);
+});
+
+// delete functionality
+// $(".remove").click(function () {
+//   console.log($(this).parent());
+//   $(this).parent().css("display", "none");
+//   $(this).parent().hide();
+//   console.log("working");
+// });
+
+$(".result").on("click", ".remove", function () {
+  $(this).parent().remove(); // Remove the parent div
+});
